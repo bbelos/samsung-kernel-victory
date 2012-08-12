3f08c15fbac91af8606df466944cebdb2a8ce274
Brian Beloshapka <bbelos@hotmail.com>
2012-08-11 22:34:16 -0500

    VICTORY: BATTERY: Additional sysfs fixes.

--- include/linux/mfd/max8998.h
+++ include/linux/mfd/max8998.h
@@ -114,6 +114,16 @@ struct s5p_batt_block_temp
 	int temp_low_recover_lpm;
 	int temp_high_event_block;
 };
+/*
+ * ADC Channel 
+ */
+struct adc_channel_type_old {
+        int s3c_adc_voltage;
+        int s3c_adc_chg_current;
+        int s3c_adc_temperature;
+        int s3c_adc_v_f;
+        int s3c_adc_hw_version;
+};
 
 /**
  * struct max8998_board - packages regulator init data
@@ -159,6 +169,7 @@ struct max8998_platform_data {
 	bool				wakeup;
 	bool				rtc_delay;
 	struct max8998_charger_data	*charger;
+	struct adc_channel_type_old		*s3c_adc_channel;
     struct s5p_batt_block_temp      *s5pc110_batt_block_temp;
 };
 
